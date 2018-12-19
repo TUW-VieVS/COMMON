07b22146d4fd2482908ca0f433dbcfc92d432285
Sigrid Böhm <sigrid.boehm@tuwien.ac.at>
2018-12-19 15:13:26 +0100

    preparation to switch to COMMON repo

--- interpolation/lagint4v.m
+++ interpolation/lagint4v.m
@@ -26,6 +26,9 @@
 %   19 Jul 2011 by Lucia Plank: replace for with find
 %   16 Sep 2011 by Hana Spicakova: fixed the bug, going back from +6 days
 %   to +5 days
+%   12 Mar 2015 by Matthias Madzak: Change of epoch (index) selection. Now
+%   always 5 values before and after ar taken (if epoch is equal, value is
+%   the same anyway!).
 % *************************************************************************
 
 
@@ -36,35 +39,41 @@ YOUTs=zeros(n,1);
 
 for s=1:n
 
-N=length(X);
-YOUT = 0.0; 
+    N=length(X);
+    YOUT = 0.0; 
 
-% for i=1:(N-1)
-%     if (XINT(s) >= X(i)) && (XINT(s) < X(i+1))
-%         k=i;
+%     for i=1:(N-1)
+%         if (XINT(s) >= X(i)) && (XINT(s) < X(i+1))
+%             k=i;
+%         end
 %     end
-% end
+%     ind=find(XINT(s)>=X);
+%     k=ind(end);
     
-ind=find(XINT(s)>=X);
-k=ind(end);
-
-
-if k < 2
-    k=2;
-end
-
-if k > (N-2)
-    k=N-2;
-end
+    % new selection of proper intervals (five before , five after -> ten
+    % values always)
+    indSmallerFlip=flipud(find(X<=XINT(s)));
+    indFirst=indSmallerFlip(5);
+    indLarger=find(X>=XINT(s));
+    indLast=indLarger(5);
     
-for m = k-5:k+5
-     TERM = Y(m);
-    for j = k-5:k+5
-        if ( m ~= j) 
-             TERM = TERM * (XINT(s) - X(j))/(X(m) - X(j));
-        end 
+%     if k < 2
+%         k=2;
+%     end
+%     
+%    
+%     if k > (N-2)
+%         k=N-2;
+%     end
+    
+    for m = indFirst:indLast
+         TERM = Y(m);
+        for j = indFirst:indLast
+            if ( m ~= j) 
+                 TERM = TERM * (XINT(s) - X(j))/(X(m) - X(j));
+            end 
+        end
+            YOUT = YOUT + TERM;
     end
-        YOUT = YOUT + TERM;
-end
-YOUTs(s)=YOUT;
+    YOUTs(s)=YOUT;
 end
